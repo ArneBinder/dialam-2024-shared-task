0ae449dcff3f63ac34490376624fac3e91ee4175
Arne Binder <arne.binder@dfki.de>
2024-03-26 13:38:07 +0100

    move methods from relation_node_utils to nodeset_utils and new create_relation_nodes

--- src/utils/nodeset_utils.py
+++ src/utils/nodeset_utils.py
@@ -1,7 +1,8 @@
 import json
 import logging
 import os
-from typing import Any, Callable, Dict, Iterator, List, Tuple, TypeVar, Union
+from collections import defaultdict
+from typing import Any, Callable, Dict, Iterator, List, Optional, Tuple, TypeVar, Union
 
 import tqdm
 
@@ -73,3 +74,114 @@ def get_node_ids(node_id2node: Dict[str, Any], allowed_node_types: List[str]) ->
     return [
         node_id for node_id, node in node_id2node.items() if node["type"] in allowed_node_types
     ]
+
+
+def create_edges_from_relations(
+    relations: List[Tuple[str, str, str]],
+    edges: List[Dict[str, str]],
+) -> List[Dict[str, str]]:
+    """Create edge objects from relations.
+
+    Args:
+        relations: A list of binary relations: tuples containing the source node ID, target node ID, and relation node ID.
+        edges: A list of edge objects where each object contains the keys "fromID" and "toID".
+
+    Returns:
+        A list of edge objects where each object contains the keys "fromID" and "toID".
+    """
+    biggest_edge_id = max([int(edge["fromID"]) for edge in edges])
+    new_edges = []
+    for src_id, trg_id, rel_id in relations:
+        biggest_edge_id += 1
+        new_edges.append({"fromID": src_id, "toID": rel_id, "edgeID": str(biggest_edge_id)})
+        biggest_edge_id += 1
+        new_edges.append({"fromID": rel_id, "toID": trg_id, "edgeID": str(biggest_edge_id)})
+    return new_edges
+
+
+def create_relation_nodes_from_alignment(
+    node_id2node: Dict[str, Any],
+    node_alignments: List[Tuple[str, str]],
+    node_type: str,
+    node_text: str,
+    swap_direction: bool = False,
+) -> Tuple[List[Tuple[str, str, str]], Dict[str, Any]]:
+
+    """Create relation nodes from alignments between two nodes.
+
+    Args:
+        node_id2node: A dictionary mapping node IDs to node objects.
+        node_alignments: A list of tuples containing the alignment between two nodes.
+        node_type: The type of the nodes.
+        node_text: The text of the nodes.
+        swap_direction: A boolean indicating whether to swap the direction of the alignment
+            before creating the relation node.
+
+    Returns:
+        A tuple containing:
+         - a list of binary YA relations: tuples containing the source node ID, target node ID, and YA node ID, and
+         - a dictionary containing the newly created YA nodes as a mapping from IDs to node content.
+    """
+    biggest_node_id = max([int(node_id) for node_id in node_id2node.keys()])
+    new_node_id2node = dict()
+    relations = []
+    for src_id, trg_id in node_alignments:
+        if swap_direction:
+            src_id, trg_id = trg_id, src_id
+        biggest_node_id += 1
+        node_id = str(biggest_node_id)
+        new_node_id2node[node_id] = {
+            "id": node_id,
+            "type": node_type,
+            "text": node_text,
+        }
+        relations.append((src_id, trg_id, node_id))
+
+    return relations, new_node_id2node
+
+
+def get_binary_relations(
+    node_id2node: Dict[str, Any],
+    edges: List[Dict[str, str]],
+    allowed_node_types: Optional[List[str]] = None,
+    allowed_source_types: Optional[List[str]] = None,
+    allowed_target_types: Optional[List[str]] = None,
+) -> List[Tuple[str, str, str]]:
+    """Create binary relations from nodes, i.e. tuples containing the source node ID, target node
+    ID, and relation node ID.
+
+    Args:
+        node_id2node: A dictionary mapping node IDs to node objects.
+        edges: A list of edge objects where each object contains the keys "fromID" and "toID".
+        allowed_node_types: A list of node types to consider.
+        allowed_source_types: A list of source node types to consider.
+        allowed_target_types: A list of target node types to consider.
+
+    Returns:
+        A list of binary relations: tuples containing the source node ID, target node ID, and relation node ID.
+    """
+
+    # helper dictionaries to map source and target nodes to their corresponding target and source nodes
+    src2targets: Dict[str, List[str]] = defaultdict(list)
+    trg2sources: Dict[str, List[str]] = defaultdict(list)
+    for edge in edges:
+        src_id = edge["fromID"]
+        trg_id = edge["toID"]
+        src2targets[src_id].append(trg_id)
+        trg2sources[trg_id].append(src_id)
+
+    relations = []
+    for node_id, node in node_id2node.items():
+        # filter nodes based on allowed types
+        if allowed_node_types is not None and node["type"] not in allowed_node_types:
+            continue
+        # iterate over all source nodes ...
+        for src_id in trg2sources[node_id]:
+            src_node = node_id2node[src_id]
+            if allowed_source_types is None or src_node["type"] in allowed_source_types:
+                # ... and all target nodes
+                for trg_id in src2targets[src_id]:
+                    trg_node = node_id2node[trg_id]
+                    if allowed_target_types is None or trg_node["type"] in allowed_target_types:
+                        relations.append((src_id, trg_id, node_id))
+    return relations
