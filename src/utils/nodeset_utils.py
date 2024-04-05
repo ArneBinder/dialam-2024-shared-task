631d0a3f1868013a95ffb4dc1c45ead29cb405f6
Arne Binder <arne.binder@dfki.de>
2024-04-05 14:15:06 +0200

    fix: set allowed_max_targets = None for TA relations

--- src/utils/nodeset_utils.py
+++ src/utils/nodeset_utils.py
@@ -293,7 +293,7 @@ def get_relations(
         allowed_source_types = ["L"]
         allowed_target_types = ["L"]
         allowed_max_sources = 1
-        allowed_max_targets = 1
+        allowed_max_targets = None
     elif relation_type == "S":
         yield from get_relations(nodeset, "RA", enforce_cardinality)
         yield from get_relations(nodeset, "CA", enforce_cardinality)
