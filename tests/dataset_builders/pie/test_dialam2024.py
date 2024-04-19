c5fc9a75d5b031c84304c8da7d7682a745405bf1
Arne Binder <arne.binder@dfki.de>
2024-04-19 22:38:53 +0200

    improve tests

--- tests/dataset_builders/pie/test_dialam2024.py
+++ tests/dataset_builders/pie/test_dialam2024.py
@@ -1,24 +1,64 @@
+import json
+
 import datasets
 import pytest
 from pie_datasets import load_dataset
 from pytorch_ie.annotations import LabeledSpan
 
+from dataset_builders.pie.dialam2024.dialam2024 import PieDialAM2024
 from src.utils.nodeset2document import SimplifiedDialAM2024Document
 
+DATA_DIR = None
+# To use local data, set DATA_DIR to a local directory containing the nodeset files.
+# But note that the blacklist is applied nevertheless!
+# (change it in the dataset_builder/hf/dialam2024/dialam2024.py file, if necessary)
+# DATA_DIR = "data/dataset_excerpt"
 
-def test_hf_dataset():
-    ds = datasets.load_dataset("ArneBinder/dialam2024")
-    assert set(ds) == {"train"}
-    assert len(ds["train"]) == 1381
+
+@pytest.fixture(scope="module")
+def hf_dataset():
+    ds = datasets.load_dataset(PieDialAM2024.BASE_DATASET_PATH, data_dir=DATA_DIR)
+    return ds
+
+
+def test_hf_dataset(hf_dataset):
+    assert set(hf_dataset) == {"train"}
+    assert len(hf_dataset["train"]) == 1381
+
+
+@pytest.fixture(scope="module")
+def hf_example():
+    ds = datasets.load_dataset(PieDialAM2024.BASE_DATASET_PATH, data_dir=DATA_DIR)
+    return ds["train"][0]
+
+
+def test_hf_example(hf_example):
+    fixture_data_path = "tests/fixtures/dataset_builders/pie/dialam2024/nodeset23156.json"
+    expected_data = json.load(open(fixture_data_path, "r"))
+    assert hf_example == expected_data
+
+
+@pytest.fixture(scope="module")
+def builder():
+    return PieDialAM2024()
+
+
+def test_convert_document(builder, hf_example):
+    document = builder._generate_document(hf_example)
+    assert isinstance(document, SimplifiedDialAM2024Document)
+    assert document.id == "23156"
+    assert len(document.l_nodes) > 0
+    fist_l_node = document.l_nodes[0]
+    assert isinstance(fist_l_node, LabeledSpan)
+    assert (
+        str(fist_l_node)
+        == "Kate Forbes : I don't expect everybody on the panel to agree with me on independence"
+    )
 
 
 @pytest.fixture(scope="module")
 def dataset():
-    return load_dataset(
-        "dataset_builders/pie/dialam2024",
-        # to use local data:
-        # data_dir="data/dataset_excerpt",
-    )
+    return load_dataset("dataset_builders/pie/dialam2024", data_dir=DATA_DIR)
 
 
 def test_dataset(dataset):
@@ -33,6 +73,7 @@ def document(dataset):
 
 def test_document(document):
     assert isinstance(document, SimplifiedDialAM2024Document)
+    assert document.id == "23156"
     assert len(document.l_nodes) > 0
     fist_l_node = document.l_nodes[0]
     assert isinstance(fist_l_node, LabeledSpan)
