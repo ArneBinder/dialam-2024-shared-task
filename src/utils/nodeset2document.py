d090ee4e313ef3e150531387d9e42322ba05962b
tanikina <tatiana.anikina@dfki.de>
2024-04-19 13:26:05 +0200

    add extra checks, compare generated doc to the original nodeset

--- src/utils/nodeset2document.py
+++ src/utils/nodeset2document.py
@@ -2,7 +2,8 @@ import argparse
 import dataclasses
 import logging
 import os
-from typing import List, Optional
+from collections import defaultdict
+from typing import Any, Dict, List, Optional
 
 import pyrootutils
 from pytorch_ie import AnnotationLayer, annotation_field
@@ -156,7 +157,141 @@ def convert_to_document(
     ta_nodes = get_node_ids_by_type(nodeset, node_types=["TA"])
     doc.metadata["ta_node_ids"] = ta_nodes
 
+    validate_document(nodeset=nodeset, document=doc)
+
     return doc
+
+
+def validate_document(nodeset: Nodeset, document: SimplifiedQT30Document):
+    metadata = document.metadata
+    original = nodeset.copy()
+    # check that we have the same L-nodes as in the original nodeset
+    orig_l_node_id2text = dict()
+    for n in original["nodes"]:
+        if n["type"] == "L":
+            orig_l_node_id2text[n["nodeID"]] = n["text"]
+    l_node_ids = metadata["l_node_ids"]
+    l_node_spans = metadata["l_node_spans"]
+    converted_l_node_id2text = dict()
+    for l_node_id in l_node_ids:
+        l_span = l_node_spans[l_node_id]
+        converted_l_node_id2text[l_node_id] = document.text[l_span.start : l_span.end]
+    for l_id in orig_l_node_id2text:
+        if l_id in converted_l_node_id2text:
+            assert converted_l_node_id2text[l_id] == orig_l_node_id2text[l_id]
+        else:
+            raise ValueError(f"L-node missing in the converted document, nodeID: {l_id}")
+    # check that we have the same TA-nodes as in the original nodeset
+    ta_node_ids = metadata["ta_node_ids"]
+    orig_ta_node_ids = [n["nodeID"] for n in original["nodes"] if n["type"] == "TA"]
+    ta_node_diff = set(orig_ta_node_ids) - set(ta_node_ids)
+    if len(ta_node_diff) != 0:
+        raise ValueError(f"TA nodes missing in the converted document: {ta_node_diff}")
+
+    # helper structures
+    orig_src2trg = defaultdict(list)
+    orig_trg2src = defaultdict(list)
+    for e in original["edges"]:
+        orig_src2trg[e["fromID"]].append(e["toID"])
+    for e in original["edges"]:
+        orig_trg2src[e["toID"]].append(e["fromID"])
+    node_id2type = {n["nodeID"]: n["type"] for n in original["nodes"]}
+
+    orig_mappings = {
+        "orig_src2trg": orig_src2trg,
+        "orig_trg2src": orig_trg2src,
+        "node_id2type": node_id2type,
+    }
+
+    # check YA I2L relations are the same as in the original nodeset
+    ya_i2l_relations = metadata["ya_i2l_relations"]
+    allowed_types = {
+        "allowed_node_types": ["YA"],
+        "allowed_source_types": ["L"],
+        "allowed_target_types": ["I"],
+    }
+    check_relations(
+        original=original,
+        relations=ya_i2l_relations,
+        orig_mappings=orig_mappings,
+        allowed_types=allowed_types,
+    )
+    # check YA S2TA relations are the same as in the original nodeset
+    ya_s2ta_relations = metadata["ya_s2ta_relations"]
+    allowed_types = {
+        "allowed_node_types": ["YA"],
+        "allowed_source_types": ["TA"],
+        "allowed_target_types": ["S"],
+    }
+    check_relations(
+        original=original,
+        relations=ya_s2ta_relations,
+        orig_mappings=orig_mappings,
+        allowed_types=allowed_types,
+    )
+    # check S relations are the same as in the original nodeset
+    s_relations = metadata["s_relations"]
+    allowed_types = {
+        "allowed_node_types": ["RA", "MA", "CA"],
+        "allowed_source_types": ["I"],
+        "allowed_target_types": ["I"],
+    }
+    check_relations(
+        original=original,
+        relations=s_relations,
+        orig_mappings=orig_mappings,
+        allowed_types=allowed_types,
+    )
+
+
+def check_relations(
+    original: Nodeset,
+    relations: List[Dict[str, Any]],
+    orig_mappings: Dict[str, Any],
+    allowed_types: Dict[str, List[str]],
+):
+    orig_src2trg = orig_mappings["orig_src2trg"]
+    orig_trg2src = orig_mappings["orig_trg2src"]
+    node_id2type = orig_mappings["node_id2type"]
+    allowed_node_types = allowed_types["allowed_node_types"]
+    allowed_source_types = allowed_types["allowed_source_types"]
+    allowed_target_types = allowed_types["allowed_target_types"]
+    # filter out only those node IDs where sources and targets have the correct (allowed) types
+    orig_node_ids = []
+    for n in original["nodes"]:
+        if n["type"] in allowed_node_types:
+            n_id = n["nodeID"]
+            n_sources = orig_src2trg[n_id]
+            n_targets = orig_trg2src[n_id]
+            n_sources_filtered = [
+                n_s for n_s in n_sources if node_id2type[n_s] in allowed_source_types
+            ]
+            n_targets_filtered = [
+                n_t for n_t in n_targets if node_id2type[n_t] in allowed_target_types
+            ]
+            if len(n_sources_filtered) > 0 and len(n_targets_filtered) > 0:
+                orig_node_ids.append(n_id)
+    node_src_trg = dict()
+    for rel in relations:
+        node_src_trg[rel["relation"]] = {"sources": rel["sources"], "targets": rel["targets"]}
+    # check for each relation whether we are missing the original source or target nodes
+    for n_id in orig_node_ids:
+        if not (n_id in node_src_trg):
+            raise ValueError(
+                f"{node_id2type[n_id]} node is not in the converted document, nodeID: {n_id}"
+            )
+        orig_sources = orig_trg2src[n_id]
+        for orig_source in orig_sources:
+            if node_id2type[orig_source] in allowed_source_types and not (
+                orig_source in node_src_trg[n_id]["sources"]
+            ):
+                raise ValueError(f"Source is missing, nodeID: {orig_source} for nodeID: {n_id}")
+        orig_targets = orig_src2trg[n_id]
+        for orig_target in orig_targets:
+            if node_id2type[orig_target] in allowed_target_types and not (
+                orig_target in node_src_trg[n_id]["targets"]
+            ):
+                raise ValueError(f"Target is missing, nodeID: {orig_target} for nodeID: {n_id}")
 
 
 def main(
