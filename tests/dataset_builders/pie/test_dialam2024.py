9f12c63e6b761567ba7ab00c116d4dcdfcdf6483
Arne Binder <arne.binder@dfki.de>
2024-04-26 17:59:57 +0200

    adjust tests

--- tests/dataset_builders/pie/test_dialam2024.py
+++ tests/dataset_builders/pie/test_dialam2024.py
@@ -25,19 +25,23 @@ def hf_dataset():
 
 
 def test_hf_dataset(hf_dataset):
-    assert set(hf_dataset) == {"train", "test"}
-    assert len(hf_dataset["train"]) == 1400
+    split_sizes = {split: len(hf_dataset[split]) for split in hf_dataset}
+    assert split_sizes == {"train": 1400, "test": 11}
+
+
+@pytest.fixture(scope="module", params=["train", "test"])
+def split_name(request):
+    return request.param
 
 
 @pytest.fixture(scope="module")
-def hf_example():
-    ds = datasets.load_dataset(PieDialAM2024.BASE_DATASET_PATH, data_dir=DATA_DIR)
-    return ds["train"][0]
+def hf_example(hf_dataset, split_name):
+    return hf_dataset[split_name][0]
 
 
-def test_hf_example(hf_example):
+def test_hf_example(hf_example, split_name):
     fixture_data_path = (
-        f"tests/fixtures/dataset_builders/pie/dialam2024/nodeset{hf_example['id']}.json"
+        f"tests/fixtures/dataset_builders/pie/dialam2024/{split_name}/{hf_example['id']}.json"
     )
     expected_data = json.load(open(fixture_data_path, "r"))
     assert hf_example == expected_data
@@ -53,53 +57,59 @@ def builder(config_name):
     return PieDialAM2024(name=config_name)
 
 
-def assert_document(document, config_name):
-    if config_name == "default":
-        assert isinstance(document, SimplifiedDialAM2024Document)
-        assert document.id == "17918"
-        assert len(document.l_nodes) == 18
-        fist_l_node = document.l_nodes[0]
-        assert len(document.ya_i2l_nodes) == 18
-        assert len(document.ya_s2ta_nodes) == 17
-        assert len(document.s_nodes) == 17
-        last_s_node = document.s_nodes[-1]
-        assert last_s_node.resolve() == (
-            "Default Rephrase",
-            (
-                ("source", ("L", "Andy Burnham : It is a mixed picture")),
-                ("target", ("L", "Fiona Bruce : What's happening where you are")),
-            ),
+def assert_document(document, config_name, split_name):
+    if split_name == "train":
+        if config_name == "default":
+            assert isinstance(document, SimplifiedDialAM2024Document)
+            assert document.id == "17918"
+            assert len(document.l_nodes) == 18
+            fist_l_node = document.l_nodes[0]
+            assert len(document.ya_i2l_nodes) == 18
+            assert len(document.ya_s2ta_nodes) == 17
+            assert len(document.s_nodes) == 17
+            last_s_node = document.s_nodes[-1]
+            assert last_s_node.resolve() == (
+                "Default Rephrase",
+                (
+                    ("source", ("L", "Andy Burnham : It is a mixed picture")),
+                    ("target", ("L", "Fiona Bruce : What's happening where you are")),
+                ),
+            )
+        elif config_name == "merged_relations":
+            assert isinstance(document, TextDocumentWithLabeledEntitiesAndNaryRelations)
+            assert document.id == "17918"
+            assert len(document.labeled_spans) == 18
+            fist_l_node = document.labeled_spans[0]
+            assert len(document.nary_relations) == 52
+            last_nary_relation = document.nary_relations[-1]
+            assert last_nary_relation.resolve() == (
+                "s_nodes:Default Rephrase",
+                (
+                    ("s_nodes:source", ("L", "Andy Burnham : It is a mixed picture")),
+                    ("s_nodes:target", ("L", "Fiona Bruce : What's happening where you are")),
+                ),
+            )
+        else:
+            raise ValueError(f"Unknown config name {config_name}")
+
+        assert isinstance(fist_l_node, LabeledSpan)
+        assert (
+            str(fist_l_node)
+            == "Claire Cooper : Even if some children do get back to school before the end of the summer term, "
+            "their experience is more likely to be about social distancing and hygiene rather than anything "
+            "of any educational value"
         )
-    elif config_name == "merged_relations":
-        assert isinstance(document, TextDocumentWithLabeledEntitiesAndNaryRelations)
-        assert document.id == "17918"
-        assert len(document.labeled_spans) == 18
-        fist_l_node = document.labeled_spans[0]
-        assert len(document.nary_relations) == 52
-        last_nary_relation = document.nary_relations[-1]
-        assert last_nary_relation.resolve() == (
-            "s_nodes:Default Rephrase",
-            (
-                ("s_nodes:source", ("L", "Andy Burnham : It is a mixed picture")),
-                ("s_nodes:target", ("L", "Fiona Bruce : What's happening where you are")),
-            ),
-        )
+    elif split_name == "test":
+        # TODO
+        pass
     else:
-        raise ValueError(f"Unknown config name {config_name}")
-
-    assert isinstance(fist_l_node, LabeledSpan)
-    assert (
-        str(fist_l_node)
-        == "Claire Cooper : Even if some children do get back to school before the end of the summer term, "
-        "their experience is more likely to be about social distancing and hygiene rather than anything "
-        "of any educational value"
-    )
+        raise ValueError(f"Unknown split name {split_name}")
 
 
-def test_convert_document(builder, hf_example):
+def test_convert_document(builder, hf_example, split_name):
     # test nodeset cleanup and conversion to document
     document = builder._generate_document(hf_example)
-    assert_document(document, config_name=builder.config.name)
+    assert_document(document, config_name=builder.config.name, split_name=split_name)
 
 
 @pytest.fixture(scope="module")
@@ -108,14 +118,14 @@ def dataset(config_name):
 
 
 def test_dataset(dataset):
-    assert set(dataset) == {"train"}
-    assert len(dataset["train"]) == 1400
+    split_sizes = {split: len(dataset[split]) for split in dataset}
+    assert split_sizes == {"train": 1400, "test": 11}
 
 
 @pytest.fixture(scope="module")
-def document(dataset):
-    return dataset["train"][0]
+def document(dataset, split_name):
+    return dataset[split_name][0]
 
 
-def test_document(document, config_name):
-    assert_document(document, config_name)
+def test_document(document, config_name, split_name):
+    assert_document(document, config_name, split_name)
